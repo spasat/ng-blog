ff5aa292869e627a9df070faf0ee449d718e0cce
Sergiu Pasat <spasat@pentalog.fr>
2017-02-22 10:15:06 +0200

    Add Login service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthServiceService } from './auth-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor() { }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
+import { MdlModule } from 'angular2-mdl'
+import { AUTH_PROVIDERS } from 'angular2-jwt';
 
 import { AppComponent } from './app.component';
+import { BlogHeaderComponent } from './blog-header/blog-header.component';
+import { AppRoutingModule } from './app-router.module';
+import { AuthServiceService } from './auth-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogHeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    MdlModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AUTH_PROVIDERS,
+    AuthServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
